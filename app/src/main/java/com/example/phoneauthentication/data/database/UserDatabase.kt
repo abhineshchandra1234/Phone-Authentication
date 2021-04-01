a76b097ce9563cd9fce27819e347999654e5fe2b
abhinesh <abhinesh.c@eegrab.com>
2021-04-01 15:13:43 +0530

    added dao class

--- app/src/main/java/com/example/phoneauthentication/data/database/UserDatabase.kt
+++ app/src/main/java/com/example/phoneauthentication/data/database/UserDatabase.kt
@@ -1,4 +1,10 @@
 package com.example.phoneauthentication.data.database
 
-class UserDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.phoneauthentication.data.models.User
+
+
+@Database(entities = [User::class], version = 1, exportSchema = false)
+abstract class UserDatabase: RoomDatabase() {
 }
