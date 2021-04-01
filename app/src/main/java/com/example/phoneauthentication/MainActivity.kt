738c0e5a28a94faa7f82b3d0f6304d583d82b15c
abhinesh chandra <abhineshchandra1234@gmail.com>
2021-04-01 09:18:12 +0530

    first commit

---
+++ app/src/main/java/com/example/phoneauthentication/MainActivity.kt
@@ -0,0 +1,122 @@
+package com.example.phoneauthentication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.*
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+    lateinit var auth: FirebaseAuth
+    lateinit var storedVerificationId:String
+    lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
+    private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        auth=FirebaseAuth.getInstance()
+        var currentUser = auth.currentUser
+        if(currentUser != null) {
+            startActivity(Intent(applicationContext, HomeActivity::class.java))
+            finish()
+        }
+
+        btnSendOtp.setOnClickListener {
+            login()
+        }
+
+        btnVerifyOtp.setOnClickListener {
+            verifyOtp()
+        }
+
+        // Callback function for Phone Auth
+        callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                startActivity(Intent(applicationContext, HomeActivity::class.java))
+                finish()
+            }
+
+            override fun onVerificationFailed(e: FirebaseException) {
+                Toast.makeText(applicationContext, "Failed", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken
+            ) {
+
+                Log.d("TAG","onCodeSent:$verificationId")
+                storedVerificationId = verificationId
+                resendToken = token
+
+//                var intent = Intent(applicationContext,Verify::class.java)
+//                intent.putExtra("storedVerificationId",storedVerificationId)
+//                ContextCompat.startActivity(intent)
+            }
+        }
+    }
+
+    private fun verifyOtp() {
+        var otp= etEnterOtp.text.toString()
+        if(!otp.isEmpty()){
+            val credential : PhoneAuthCredential = PhoneAuthProvider.getCredential(
+                storedVerificationId.toString(), otp)
+            signInWithPhoneAuthCredential(credential)
+        }else{
+            Toast.makeText(this,"Enter OTP",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun login() {
+        var number=etEnterNo.text.toString()
+
+        if(!number.isEmpty()){
+            number="+91"+number
+            sendVerificationcode (number)
+        }else{
+            Toast.makeText(this,"Enter mobile number",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun sendVerificationcode(number: String) {
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(number) // Phone number to verify
+            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+            .setActivity(this) // Activity (for callback binding)
+            .setCallbacks(callbacks) // OnVerificationStateChangedCallbacks
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+    }
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    startActivity(Intent(applicationContext, HomeActivity::class.java))
+                    finish()
+// ...
+                } else {
+// Sign in failed, display a message and update the UI
+                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+// The verification code entered was invalid
+                        Toast.makeText(this,"Invalid OTP",Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+    }
+
+
+}
+
