1fa8a552fadb39128a3bd18ae63dc86952031120
abhinesh <abhinesh.c@eegrab.com>
2021-04-01 15:03:01 +0530

    created user table

--- app/src/main/java/com/example/phoneauthentication/data/models/User.kt
+++ app/src/main/java/com/example/phoneauthentication/data/models/User.kt
@@ -1,11 +1,13 @@
 package com.example.phoneauthentication.data.models
 
+import android.os.Parcelable
 import androidx.room.Entity
+import kotlinx.android.parcel.Parcelize
 
-
-@Entity()
+@Parcelize
+@Entity(tableName = "user_table")
 data class User(
     val user_number: String,
     val user_name: String,
     val user_email: String
-)
+) : Parcelable
