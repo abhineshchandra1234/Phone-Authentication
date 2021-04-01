5e1915621ea47a7248da3f127c15122987339e23
abhinesh <abhinesh.c@eegrab.com>
2021-04-01 18:23:12 +0530

    able to display user phone no && app is working fine

--- app/src/main/java/com/example/phoneauthentication/HomeActivity.kt
+++ app/src/main/java/com/example/phoneauthentication/HomeActivity.kt
@@ -1,22 +1,51 @@
 package com.example.phoneauthentication
 
 import android.content.Intent
+import android.content.QuickViewConstants
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.AndroidViewModel
+import com.example.phoneauthentication.data.models.User
+import com.example.phoneauthentication.viewModels.UserViewModel
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     lateinit var auth: FirebaseAuth
+    private lateinit var userViewModel: UserViewModel
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
         auth=FirebaseAuth.getInstance()
+        if (auth.currentUser != null) {
+            Log.d("TAG", "user phone no is ${auth.currentUser.phoneNumber.toString()}")
+        }
         btnLogOut.setOnClickListener {
             auth.signOut()
             startActivity(Intent(applicationContext, MainActivity::class.java))
             finish()
+        }
+
+        btnRegister.setOnClickListener {
+            addUserToDatabase()
+        }
+    }
+
+    private fun addUserToDatabase() {
+        var name = etEnterName.text.toString().trim()
+        var email = etEnterEmail.text.toString().trim()
+
+        if (!(name.isEmpty() || email.isEmpty())) {
+
+        } else {
+            Toast.makeText(applicationContext, "Please fill all the fields", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/example/phoneauthentication/data/models/User.kt
+++ app/src/main/java/com/example/phoneauthentication/data/models/User.kt
@@ -4,10 +4,10 @@ import android.os.Parcelable
 import androidx.room.Entity
 import kotlinx.android.parcel.Parcelize
 
-@Parcelize
+
 @Entity(tableName = "user_table")
 data class User(
     val user_number: String,
     val user_name: String,
     val user_email: String
-) : Parcelable
+)
